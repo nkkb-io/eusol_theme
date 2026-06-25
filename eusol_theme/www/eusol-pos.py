db12fe86b5d01f434aebfd9009305c12c18fe4c4
nkkb-io <e.kyei2406@gmail.com>
2026-06-25 22:19:22 +0000

    Update eusol-pos.py

--- eusol_theme/www/eusol-pos.py
+++ eusol_theme/www/eusol-pos.py
@@ -1,1 +1,9 @@
+import frappe
 
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.local.flags.redirect_location = "/login"
+        raise frappe.Redirect
+    context.no_cache = 1
+    context.user_name = frappe.get_value("User", frappe.session.user, "full_name") or frappe.session.user
+    context.user_initials = "".join([n[0].upper() for n in (context.user_name or "PO").split()[:2]])
